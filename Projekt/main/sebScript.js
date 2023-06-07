2414cfe4f9f32763faa0526e03f3c75c7b794555
Sebi1531 <33638099+Sebi1531@users.noreply.github.com>
2023-06-07 21:22:12 +0200

    corrections
    Korrekturen, welche im Rahmen der Besprechung am 7.6. durchgeführt wurden.

--- Projekt/main/sebScript.js
+++ Projekt/main/sebScript.js
@@ -2,7 +2,9 @@ function switchTab(tabI) {
     let tabId = "main-sect4-tab";
     let cntId = "main-sect4-tabcontainer";
     document.getElementById(tabId + (1 + tabI)).style["grid-row"] =  "2 / 4";
-    document.getElementById(cntId + (1 + tabI)).style["display"] = "block";
+    document.getElementById(tabId + (1 + tabI)).style["opacity"] = 1;
+    document.getElementById(cntId + (1 + tabI)).style["visibility"] = "visible";
     document.getElementById(tabId + (2 - tabI)).style["grid-row"] = "3";
-    document.getElementById(cntId + (2 - tabI)).style["display"] = "none";
+    document.getElementById(tabId + (2 - tabI)).style["opacity"] = 0.5;
+    document.getElementById(cntId + (2 - tabI)).style["visibility"] = "hidden";
 }
