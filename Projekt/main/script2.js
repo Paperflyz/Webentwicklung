5b1e8813de2a162ac6b3720fa97ca25983248766
PaperflyZ <calpri95@outlook.de>
2023-06-12 15:50:03 +0200

    Updated Naviagtion

--- Projekt/main/script2.js
+++ Projekt/main/script2.js
@@ -1,7 +1,15 @@
-const hamburger = document.querySelector(".icon-hamburger");
+const hamburger = document.getElementById("icon-hamburger");
+const hamburger_close = document.getElementById("icon-close");
 
 
-hamburger.addEventListener("click", (e) => {
-    e.target.classList.toggle("opened");
-    e.target.closest("nav").querySelector(".menu-slide").classList.toggle("show");
+[hamburger, hamburger_close].forEach(el => {
+    el.addEventListener("click", (e) => {
+        hamburger.classList.toggle("ds-none");
+        hamburger_close.classList.toggle("ds-none");
+        e.target.closest("nav").querySelector(".menu-slide").classList.toggle("show");
+    })
 })
+// hamburger.addEventListener("click", (e) => {
+//     e.target.classList.toggle("opened");
+//     e.target.closest("nav").querySelector(".menu-slide").classList.toggle("show");
+// })
