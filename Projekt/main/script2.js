806545f0e245258144862542b114a480c807ef99
PaperflyZ <calpri95@outlook.de>
2023-06-12 16:41:25 +0200

    initial commit on shop

--- Projekt/main/script2.js
+++ Projekt/main/script2.js
@@ -7,9 +7,24 @@ const hamburger_close = document.getElementById("icon-close");
         hamburger.classList.toggle("ds-none");
         hamburger_close.classList.toggle("ds-none");
         e.target.closest("nav").querySelector(".menu-slide").classList.toggle("show");
+
     })
 })
 // hamburger.addEventListener("click", (e) => {
 //     e.target.classList.toggle("opened");
 //     e.target.closest("nav").querySelector(".menu-slide").classList.toggle("show");
 // })
+
+fetch('./data.json')
+  .then(response => response.json())
+  .then(data => {
+    // Hier hast du Zugriff auf die Daten aus der JSON-Datei
+    console.log(data.objekte); // Beispiel: Konsolenausgabe der "objekte" in der JSON-Datei
+
+    // Weitere Verarbeitung der Daten
+    // ...
+  })
+  .catch(error => {
+    // Fehlerbehandlung, falls das Laden der JSON-Datei fehlschlägt
+    console.error('Fehler beim Laden der JSON-Datei:', error);
+  });
